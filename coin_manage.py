294550d474f2308969cb0abd3d23ecb18bca9bc1
Romario Melo Business <romariomelo.contato@gmail.com>
2024-08-30 13:26:00 -0300

    coin_manage agora tem o upgrade all coins, com downloads de forma async

--- coin_manage.py
+++ coin_manage.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import vectorbt as vbt
+import requests
+import asyncio
+from tqdm.asyncio import tqdm_asyncio
 
 COIN_BASE_PATH = 'C:/CoinsBase/1h/{}.csv'
 
@@ -24,7 +27,22 @@ class CoinManage:
                 )        
         except:
             print("Falha ao acessar a lista de símbolos.")
+
         return coins
+    
+    @staticmethod
+    def get_usdt_futures_pairs() -> list[str]:
+
+        url = "https://fapi.binance.com/fapi/v1/exchangeInfo" 
+        response = requests.get(url)
+        data = response.json()
+
+        usdt_pairs = []
+        for symbol_info in data['symbols']:
+            if symbol_info['quoteAsset'] == 'USDT': 
+                usdt_pairs.append(symbol_info['symbol'])
+
+        return usdt_pairs
     
     @staticmethod
     def download_coin(coin:Coin, start):
@@ -33,12 +51,14 @@ class CoinManage:
                 coin.name,
                 start=start, 
                 end='now UTC',
-                interval='1h'
+                interval='1h',
+
             )
 
             return binance_data.get()
         
         except:
+            print(f'Falha ao baixar {coin.name}')
             return []
         
     @staticmethod
@@ -53,7 +73,6 @@ class CoinManage:
 
             data = pd.concat([data.iloc[:-1], df_new])
             data.to_csv(coin.path)
-            print(f"{coin.name} - Arquivo local atualizado.")
 
         except:
             
@@ -61,4 +80,21 @@ class CoinManage:
 
             if len(data) > 1:
                 data.to_csv(coin.path)
-                print(f"{coin.name} - Arquivo local gerado.")
+
+    @staticmethod
+    def upgrade_all_coins():
+        coins_list = CoinManage.get_usdt_futures_pairs()
+        asyncio.run(CoinManage()._download_progress_bar(coins_list))
+
+    async def _download_async(self, coin_name):
+        await asyncio.to_thread(CoinManage.update_coin, Coin(coin_name))
+
+    async def _download_progress_bar(self, coins_list):
+        tasks = [self._download_async(coin_name) for coin_name in coins_list]
+        for f in tqdm_asyncio.as_completed(tasks, total=len(tasks), desc="Baixando dados das moedas"):
+            await f
+
+    
+
+if __name__=='__main__':
+    CoinManage.upgrade_all_coins()
